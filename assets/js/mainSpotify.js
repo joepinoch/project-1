4958c6fc282100c2a1d1225423ca9179531679d2
Mgruber96 <toupe454@live.com>
2023-04-05 21:30:48 -0700

    Modifications to JS and HTML

--- assets/js/mainSpotify.js
+++ assets/js/mainSpotify.js
@@ -20,62 +20,93 @@ var client_secret = '313a1f6cf1494efc998665ef968bb252';
 //   }
 // });
 
-fetch('https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token', {
-    method: 'POST',
-    headers: {
-        'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
- //       'Content-Type': 'application/json'
-      },
-}).then(response => {
-    return response.json();
-}).then(data => {
-    console.log(data);
-});
+// fetch('https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token', {
+//     method: 'POST',
+//     headers: {
+//         'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
+//  //       'Content-Type': 'application/json'
+//       },
+// }).then(response => {
+//     return response.json();
+// }).then(data => {
+//     console.log(data);
+// });
 
 
-/* I still need to receive the access token
+const clientId = "eb6f3c00e7284fc8a663afe965bf07f4"
+const clientSecret = "313a1f6cf1494efc998665ef968bb252"
+let token = "";
+
+async function getToken() {
+
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        body: 'grant_type=client_credentials&client_id=' + clientId + '&client_secret=' + clientSecret,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+    });
+    let res = await response.json()
+    console.log(res);
+    token = res.access_token
+
+}
+
+getToken()
+setInterval(getToken, 3600000)
+
+
+
+
+async function exampleSpotifyCall() {
+    const response = await fetch(`https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb`, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    })
+    let res = await response.json()
+    console.log(res)
+}
+// exampleSpotifyCall();
+
+setTimeout(exampleSpotifyCall, 5000);
+
+
 {
-    "access_token": "NgCXRKc...MzYjw",
-    "token_type": "bearer",
-    "expires_in": 3600
- }
- */
- const clientId = "eb6f3c00e7284fc8a663afe965bf07f4"
- const clientSecret = "313a1f6cf1494efc998665ef968bb252"
- let token = "";
- 
- async function getToken() {
-  
-     const response = await fetch('https://accounts.spotify.com/api/token', {
-         method: 'POST',
-         body: 'grant_type=client_credentials&client_id=' + clientId + '&client_secret=' + clientSecret,
-         headers: {
-             'Content-Type': 'application/x-www-form-urlencoded'
-         }
-        
-     });
-      let res = await response.json()
-      console.log(res);
-      token = res.access_token
-     
- }
- 
- getToken()
- setInterval(getToken, 3600000)
- 
- 
- 
- 
- async function exampleSpotifyCall() {
-     const response = await fetch(`https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb`, {
-         method: "GET",
-         headers: {
-           "Authorization": `Bearer ${token}`
-         }
-     })
-     let res= await response.json()
-     console.log(res)
- }
- // exampleSpotifyCall();
-
- setTimeout(exampleSpotifyCall, 5000);
+    "href": "https://api.spotify.com/v1/me/shows?offset=0&limit=20",
+        "limit": 10,
+            "next": "https://api.spotify.com/v1/me/shows?offset=1&limit=1",
+                "offset": 0,
+                    "previous": "https://api.spotify.com/v1/me/shows?offset=1&limit=1",
+                        "total": 4,
+                            "items": [
+                                {
+                                    "external_urls": {
+                                        "spotify": "string"
+                                    },
+                                    "followers": {
+                                        "href": "string",
+                                        "total": 0
+                                    },
+                                    "genres": [
+                                        "Prog rock",
+                                        "Grunge"
+                                    ],
+                                    "href": "string",
+                                    "id": "string",
+                                    "images": [
+                                        {
+                                            "url": "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228",
+                                            "height": 300,
+                                            "width": 300
+                                        }
+                                    ],
+                                    "name": "string",
+                                    "popularity": 0,
+                                    "type": "artist",
+                                    "uri": "string"
+                                }
+                            ]
+}
--- assets/js/mainTicketmaster.js
+++ assets/js/mainTicketmaster.js
@@ -1,28 +1,44 @@
-var location = document.getElementById("search-location");
-console.log(location);
-var genre = document.getElementById("genre");
-console.log(genre);
 
 
-$(document).ready(function(){
-  $("button").on("click", function(){
+
+$(document).ready(function () {
+  $("button").on("click", function () {
+    event.preventDefault();
+    var Searchlocation = document.getElementById("search-location").value;
+    console.log(Searchlocation);
+    var genre = document.getElementById("genre").value;
+    console.log(genre);
+    console.log(Searchlocation);
+    console.log(genre);
     $.ajax({
-      type:"GET",
-      url:`https://app.ticketmaster.com/discovery/v2/events.json?size=10&postalCode=${location.val}&classificationName=[music,${genre.val}]&apikey=fRxWNBut35gtcCbvmA6gavOYvhqsLzKQ`,
-      async:true,
+      type: "GET",
+      url: `https://app.ticketmaster.com/discovery/v2/events.json?size=10&postalCode=${Searchlocation}&classificationName=[music,${genre}]&apikey=fRxWNBut35gtcCbvmA6gavOYvhqsLzKQ`,
+      async: true,
       dataType: "json",
-      success: function(json) {
-                  console.log(json._embedded.events);
-                  for (i=0,i=<json._embedded.events.length,i++) {
-                    var artist[i] = json._embedded.events
-                  }
-                  // Parse the response.
-                  // Do other things.
-               },
-      error: function(xhr, status, err) {
-                  console.log("it no work")
-                  // This time, we do not end up here!
-               }
+      success: function (json) {
+        console.log(json._embedded.events);
+        //let artist = new Array(json._embedded.events.length);
+        for (let i = 0; i <= json._embedded.events.length; i++) {
+          var artist = json._embedded.events[i]._embedded.attractions[0].artist || "No Artist";
+          //if (json._embedded.events[i]._embedded.attractions[i]=false) {
+          // artist[i]="Artist Name not Available";
+          // } else {
+          // artist[i] = json._embedded.events[i]._embedded.attractions[i]; 
+          //}
+
+        }
+        console.log(artist);
+
+      },
+
+      // Parse the response.
+      // Do other things.
+      //  }),
+      error: function (xhr, status, err) {
+        console.log("it no work")
+        // This time, we do not end up here!
+      }
     });
   });
 });
+[0]
