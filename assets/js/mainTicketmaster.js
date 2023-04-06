130bbcffdeaa26063f2e5832f92460838558e450
ortiz MJ <ortizzzj99@gmail.com>
2023-04-05 22:05:17 -0700

    got images rendered and a more info ancor tag

--- assets/js/mainTicketmaster.js
+++ assets/js/mainTicketmaster.js
@@ -16,18 +16,40 @@ $(document).ready(function () {
       async: true,
       dataType: "json",
       success: function (json) {
-        console.log(json._embedded.events);
-        //let artist = new Array(json._embedded.events.length);
-        for (let i = 0; i <= json._embedded.events.length; i++) {
-          var artist = json._embedded.events[i]._embedded.attractions[0].artist || "No Artist";
-          //if (json._embedded.events[i]._embedded.attractions[i]=false) {
-          // artist[i]="Artist Name not Available";
-          // } else {
-          // artist[i] = json._embedded.events[i]._embedded.attractions[i]; 
-          //}
+        console.log(json);
 
+        var events = json._embedded.events;
+
+        for (var i = 0; i < events.length; i++) {
+          console.log(events[i]);
+          var titleEl = document.createElement('h2');
+          var moreInfoEl = document.createElement('a');
+          var imageEl = document.createElement('img');
+          
+          titleEl.textContent = events[i].name;
+          moreInfoEl.textContent = 'More info >';
+
+          moreInfoEl.setAttribute('href', events[i].url);
+          moreInfoEl.setAttribute('target', '_blank');
+
+          imageEl.setAttribute('src', events[i].images[0].url);
+
+          document.querySelector('#results').appendChild(titleEl);
+          document.querySelector('#results').appendChild(moreInfoEl);
+          document.querySelector('#results').appendChild(imageEl);
         }
-        console.log(artist);
+        // console.log(json._embedded.events);
+        //let artist = new Array(json._embedded.events.length);
+        // for (let i = 0; i <= json._embedded.events.length; i++) {
+        //   var artist = json._embedded.events[i]._embedded.attractions[0].artist || "No Artist";
+        //   //if (json._embedded.events[i]._embedded.attractions[i]=false) {
+        //   // artist[i]="Artist Name not Available";
+        //   // } else {
+        //   // artist[i] = json._embedded.events[i]._embedded.attractions[i]; 
+        //   //}
+
+        // }
+        // console.log(artist);
 
       },
 
